0f1db98a59a8917e4acdf3d9ba53b716d120f2fb
swamy411 <76993596+swamy411@users.noreply.github.com>
2021-04-17 09:25:29 +0000

    Update build.sh

--- devops/scripts/build.sh
+++ devops/scripts/build.sh
@@ -50,7 +50,8 @@ compile_lambdas() {
         cd "${CI_PROJECT_DIR}/lambda_functions/dev-lambdas/$folder" || exit
         echo "Lambda DIR"
         echo $(pwd)
-        "C:\Program Files\nodejs\node.exe" npm install .
+#         "C:\Program Files\nodejs\node.exe" npm install .
+        "C:\Program Files\nodejs\node_modules\npm\scripts" npm install .
         echo "Packaging Lambda Artifacts"
         mkdir -p "${CI_PROJECT_DIR}/artifacts/lambdas"
         #zip -r -j "${CI_PROJECT_DIR}/artifacts/lambdas/${folder}.zip" .
