4f2ba38006d42dc56347749f5140895b983a8289
swamy411 <76993596+swamy411@users.noreply.github.com>
2021-04-17 13:21:42 +0000

    Update index.js

--- lambda_functions/dev-lambdas/dev_ClearExistingExportZip/index.js
+++ lambda_functions/dev-lambdas/dev_ClearExistingExportZip/index.js
@@ -18,7 +18,7 @@ exports.handler = (event) => {
 
         s3.listObjects(params, function (err, data) {
             if (err) {
-                console.log(err, err.stack); // an error occurred
+                console.log(err, err.stack); //an error occurred
             } else {
 
                 if (data['Contents'] != undefined) {
