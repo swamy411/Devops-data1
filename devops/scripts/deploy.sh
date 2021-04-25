d0443e6bd67b50b638eb97556f9f56a955825f9a
swamy411 <76993596+swamy411@users.noreply.github.com>
2021-04-25 16:47:37 +0530

    Update deploy.sh

--- devops/scripts/deploy.sh
+++ devops/scripts/deploy.sh
@@ -4,6 +4,6 @@ CI_PROJECT_DIR=$1
 cd "${CI_PROJECT_DIR}" || exit
 echo "[Info] Creating/Updating Cloud Formation Stack : ${STACK_NAME}"
 
-# "C:\Program Files\Amazon\AWSCLIV2\aws.exe" cloudformation "${OPERATION}-stack" --stack-name "${STACK_NAME}" --template-body file://devops/cloudformation/lambdas.yaml --region "${REGION_NAME}" --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM
+"C:\Program Files\Amazon\AWSCLIV2\aws.exe" cloudformation "${OPERATION}-stack" --stack-name "${STACK_NAME}" --template-body file://devops/cloudformation/lambdas.yaml --region "${REGION_NAME}" --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM
 
-aws cloudformation deploy --template-file file://devops/cloudformation/lambdas.yaml --region $REGION_NAME --stack-name $STACK_NAME --capabilities CAPABILITY_IAM
+# aws cloudformation deploy --template-file file://devops/cloudformation/lambdas.yaml --region $REGION_NAME --stack-name $STACK_NAME --capabilities CAPABILITY_IAM
