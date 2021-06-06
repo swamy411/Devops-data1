43e0af039efa32a4cf9b675881067a51da13aebe
swamy411 <vijjapubhairavaswamy@gmail.com>
2021-06-06 19:36:26 +0530

    generate signed url script added

--- devops/scripts/build.sh
+++ devops/scripts/build.sh
@@ -4,6 +4,7 @@
 CI_PROJECT_DIR=$1
 COMMIT_SHA=$( git rev-parse HEAD )
 
+FFMPEG_LIB_DIR=("ffmpeg_lib")
 
 #DEFAULTS
 #Ignore Other Folders to Ignore During Build.
@@ -16,7 +17,7 @@ ignore_upload=("upload.sh")
 
 # #Identify the Affected Lambdas
 
-mapfile -t lines < <(git diff-tree --no-commit-id --name-only -r "${COMMIT_SHA}" | grep ./ | cut -d/ -f3 | uniq )
+mapfile -t lines < <(git diff-tree --no-commit-id --name-only -r "${COMMIT_SHA}" | grep ./ | cut -d/ -f2 | uniq )
 
 # mapfile -t lines < <(git diff-tree --no-commit-id --name-only -r "${COMMIT_SHA}" | grep ./ | tr "/" " "| uniq )
 
@@ -25,9 +26,6 @@ echo "Lines Value---: ${lines}"
 affected_folders=("$(printf '%s\n' "${lines[@]}" | sort -r)")
 echo "Affected Folders : ${affected_folders[*]}"
 
-#Testing
-#affected_folders=("lambda-B")
-echo "Affected Folders : ${affected_folders[*]}"
 
 build_all_lambdas=false
 
@@ -38,12 +36,26 @@ fi
 
 echo "Build All Lambdas ? ${build_all_lambdas}"
 
+deploy_ffmpeg() {
+    echo "ffmpeg Folder Name :--- ${FFMPEG_LIB_DIR}"
+    cd "${CI_PROJECT_DIR}/dependencies/$FFMPEG_LIB_DIR" || continue
+    
+    echo $(ls ${CI_PROJECT_DIR}/dependencies/$FFMPEG_LIB_DIR)
+    echo "Packaging Lambda Layer Artifacts"
+    
+    "C:\Program Files\WinRAR\WinRAR.exe" a -afzip -r -y "ffmpeg.zip" .
+    echo $(ls ${CI_PROJECT_DIR}/dependencies/$FFMPEG_LIB_DIR)
+
+    echo "Deploying ffmpeg library...."
+    "C:\Program Files\Amazon\AWSCLIV2\aws.exe" lambda publish-layer-version --layer-name ffmpeg_custom_layer --description "Custom ffmpeg layer" --compatible-runtimes nodejs14.x --zip-file "fileb://ffmpeg.zip"
+}
+
 compile_lambdas() {
     src_path=( $@ )
     echo $@
     echo "SRC PATH @ : ${src_path[@]}"
     echo $(pwd)
-    ListDir=$(ls ${CI_PROJECT_DIR}/lambda_functions/${STAGE}-lambdas)
+    ListDir=$(ls ${CI_PROJECT_DIR}/lambda-functions)
     echo "List Dir:-- ${ListDir[@]}"
     cd "${CI_PROJECT_DIR}" || exit
     for folder in ${src_path[@]}; 
@@ -56,30 +68,28 @@ compile_lambdas() {
             exit
         fi
         
-        echo "Folder Name :--- ${folder}"
-        cd "${CI_PROJECT_DIR}/lambda_functions/${STAGE}-lambdas/$folder" || exit
-        npm install
+        if [ "${folder}" == ${FFMPEG_LIB_DIR} ]; then 
+            deploy_ffmpeg
+        else
+            echo "Folder Name :--- ${folder}"
+            cd "${CI_PROJECT_DIR}/lambda-functions/$folder" || continue
+            npm install
 
-        echo $(ls ${CI_PROJECT_DIR}/lambda_functions/${STAGE}-lambdas/$folder)
-        # sleep 20 &
+            echo $(ls ${CI_PROJECT_DIR}/lambda-functions/$folder)
+            echo "Packaging Lambda Artifacts"
+            mkdir -p "${CI_PROJECT_DIR}/artifacts/${STAGE}_lambdas"
+            "C:\Program Files\WinRAR\WinRAR.exe" a -afzip -r -y "${CI_PROJECT_DIR}/artifacts/${STAGE}_lambdas/${STAGE}_${folder}.zip" .
+        fi
 
-        # wait $! 
-
-        echo "Packaging Lambda Artifacts"
-        mkdir -p "${CI_PROJECT_DIR}/artifacts/lambdas"
-        #zip -r -j "${CI_PROJECT_DIR}/artifacts/lambdas/${folder}.zip" .
-        "C:\Program Files\WinRAR\WinRAR.exe" a -afzip -r -y "${CI_PROJECT_DIR}/artifacts/lambdas/${folder}.zip" .
-        # "C:\Program Files\7-Zip\7z.exe" a -tzip "${CI_PROJECT_DIR}/artifacts/lambdas/${folder}.zip" "${CI_PROJECT_DIR}/artifacts/lambdas"
-        # "C:\Program Files\WinRAR\Rar.exe" a -r -y -tzip "${CI_PROJECT_DIR}/artifacts/lambdas/${folder}.zip" .
-        
         
     done;
 }
 
 if [ "$build_all_lambdas" = true ] ; then
+    deploy_ffmpeg
     echo 'Building All Lambdas'
-    cd "${CI_PROJECT_DIR}/lambda_functions/${STAGE}-lambdas" || exit
-    LAMBDAS=$( ls -d ${STAGE}_* )
+    cd "${CI_PROJECT_DIR}/lambda-functions" || exit
+    LAMBDAS=$( ls )
     compile_lambdas "${LAMBDAS[@]}"
 else
     echo "Compiling below lambdas: ${affected_folders[*]}"
--- devops/scripts/upload.sh
+++ devops/scripts/upload.sh
@@ -9,25 +9,36 @@ echo "Uploading Artifacts"
 #aws s3 cp "${CI_PROJECT_DIR}/artifacts/" "s3://${S3_BUCKET}/" --recursiv
 "C:\Program Files\Amazon\AWSCLIV2\aws.exe" s3 cp "${CI_PROJECT_DIR}/artifacts/" "s3://${S3_BUCKET}/" --recursive
 
-cp -f "${CI_PROJECT_DIR}/devops/cloudformation/lambdas-version.yaml" "${CI_PROJECT_DIR}/devops/cloudformation/lambdas.yaml"
+# mkdir -p "${CI_PROJECT_DIR}/devops/${STAGE}_cloudformation"
 
-sed -i "s/ENV/${STAGE}/g" "${CI_PROJECT_DIR}/devops/cloudformation/lambdas.yaml"
+# cp -f "${CI_PROJECT_DIR}/devops/cloudformation/lambdas-version.yaml" "${CI_PROJECT_DIR}/devops/${STAGE}_cloudformation/${STAGE}_lambdas.yaml"
+
+# sed -i "s/ENV/${STAGE}/g" "${CI_PROJECT_DIR}/devops/${STAGE}_cloudformation/${STAGE}_lambdas.yaml"
+
+# echo "Fetching Lambda Layer version for ffmpeg"
+# Layer_Arn=$("C:\Program Files\Amazon\AWSCLIV2\aws.exe" lambda list-layer-versions --layer-name ffmpeg_custom_layer --query LayerVersions[0].LayerVersionArn)
+
+# echo "Layer Arn : ${Layer_Arn}"
+# sed -i "s/ffmpeg_custom_layer_arn/${Layer_Arn}/g" "${CI_PROJECT_DIR}/devops/${STAGE}_cloudformation/${STAGE}_lambdas.yaml"
 
 # Update Lambda S3 Version in CF Template
 echo "uploading script-.--............"
-cd "${CI_PROJECT_DIR}/lambda_functions/${STAGE}-lambdas" || exit
+cd "${CI_PROJECT_DIR}/lambda-functions" || exit
 echo $(pwd)
 echo $(ls)
 #lambdas_list=$( ls -d */ | grep "lambda-" | cut -d / -f1 )
-lambdas_list=$( ls -d ${STAGE}_* )
+lambdas_list=$( ls )
 for lambda in ${lambdas_list[@]};
 do
-    VERSION_ID=$("C:\Program Files\Amazon\AWSCLIV2\aws.exe" s3api put-object-tagging --bucket "${S3_BUCKET}" --key "lambdas/${lambda}.zip" --tagging 'TagSet=[{Key=lambda,Value=getVersion}]' --output text)
+    # VERSION_ID=$("C:\Program Files\Amazon\AWSCLIV2\aws.exe" s3api put-object-tagging --bucket "${S3_BUCKET}" --key "${STAGE}_lambdas/${STAGE}_${lambda}.zip" --tagging 'TagSet=[{Key=lambda,Value=getVersion}]' --output text)
+    SIGNED_URL=$("C:\Program Files\Amazon\AWSCLIV2\aws.exe" aws s3 presign s3://${S3_BUCKET}/${STAGE}_lambdas/${STAGE}_${lambda}.zip  --expires-in 250000)
     
-    echo "$lambda : $VERSION_ID"
-    sed -i "s/<${lambda}-s3-version>/${VERSION_ID}/g" "${CI_PROJECT_DIR}/devops/cloudformation/lambdas.yaml"
+
+    echo "$lambda : $SIGNED_URL"
+    # sed -i "s/<${STAGE}_${lambda}-s3-version>/${VERSION_ID}/g" "${CI_PROJECT_DIR}/devops/${STAGE}_cloudformation/${STAGE}_lambdas.yaml"
     
 done;
-
+# uploading sam template file to S3....
+# "C:\Program Files\Amazon\AWSCLIV2\aws.exe" s3 cp "${CI_PROJECT_DIR}/devops/${STAGE}_cloudformation/" "s3://${TEMPLATE_S3_BUCKET}/" --recursive
 # echo "Uploading cloudformation files"
 # aws s3 cp "${CI_PROJECT_DIR}/devops/cloudformation/" "s3://${S3_BUCKET}/" --recursive
